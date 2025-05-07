b679cda275d7d3e2bc416de6218a19c665dde5b2
Ngoc Tran <tranbui.290703@gmail.com>
2025-05-07 14:50:52 +0700

    Update urls.py

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from . import views
 
-app_name = 'frontend'  
+app_name = 'frontend'
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    path('book-ride/', views.book_ride, name='book_ride'),
+    path('about/', views.about, name='about'),
     path('register-driver/', views.register_driver, name='register_driver'),
-    path('logout/', views.logout_view, name='logout'),
+    path('history/', views.history, name='history'),
+    path('driver-dashboard/', views.driver_dashboard, name='driver_dashboard'),
+    path('book-ride/', views.book_ride, name='book_ride'),
 ]

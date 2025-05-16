12e64258c41d830bb591484e0de8322827f448f4
Ngoc Tran <tranbui.290703@gmail.com>
2025-05-16 14:06:50 +0700

    Update models.py

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    is_driver = models.BooleanField(default=False)
+    id_number = models.CharField(max_length=20, blank=True, null=True)
+    driver_license = models.ImageField(upload_to='driver_licenses/', blank=True, null=True)
+    vehicle_photo = models.ImageField(upload_to='vehicle_photos/', blank=True, null=True)
+    is_connected = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
